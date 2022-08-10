7af202f41c03ca8642b00ef2777544ed326c3cc4
LUSEDOU <dolorier_uribe@hotmail.com>
2022-08-09 21:20:32 -0500

    feat(gallery): create view

--- lib/gallery/view/gallery_page.dart
+++ lib/gallery/view/gallery_page.dart
@@ -6,6 +6,10 @@ import 'package:super_gallery/gallery/gallery.dart';
 class GalleryPage extends StatelessWidget {
   const GalleryPage({super.key});
 
+  static Route<void> route() {
+    return MaterialPageRoute<void>(builder: (_) => const GalleryPage());
+  }
+
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
@@ -20,8 +24,58 @@ class GalleryPage extends StatelessWidget {
 class _GalleryView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    List<Card> _buildGridCards(int count) {
+      final cards = List.generate(
+        count,
+        (int index) {
+          return Card(
+            clipBehavior: Clip.antiAlias,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                AspectRatio(
+                  aspectRatio: 18 / 11,
+                  child: Image.network(
+                    context.select(
+                      (GalleryCubit cubit) => cubit.state.images[index],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16, 12, 16, 8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const <Widget>[
+                      Text('Title'),
+                      SizedBox(height: 8),
+                      Text('Secondary Text'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      );
+      return cards;
+    }
+
     return Scaffold(
-      body: GridView.count(crossAxisCount: 2),
+      body: SafeArea(
+        child: BlocListener<GalleryCubit, GalleryState>(
+          listener: (context, state) {
+            // TODO: implement listener
+          },
+          child: GridView.count(
+            crossAxisCount: 2,
+            padding: const EdgeInsets.all(16),
+            childAspectRatio: 8 / 9,
+            children: _buildGridCards(
+              context.select((GalleryCubit cubit) => cubit.state.images.length),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
