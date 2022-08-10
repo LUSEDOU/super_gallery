847149903c67eec3d1e4512dc40ce21ea970a124
LUSEDOU <dolorier_uribe@hotmail.com>
2022-08-09 19:15:50 -0500

    test(super_gallery): delete counter

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -7,13 +7,13 @@
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:super_gallery/app/app.dart';
-import 'package:super_gallery/counter/counter.dart';
+import 'package:super_gallery/upload/upload.dart';
 
 void main() {
   group('App', () {
     testWidgets('renders CounterPage', (tester) async {
       await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
+      expect(find.byType(UploadPage), findsOneWidget);
     });
   });
 }
